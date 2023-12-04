25cac71374ce61b88552bb753d6088ff88137b31
Ecem Ozturk <ecemozturk704@gmail.com>
2023-12-04 14:51:58 +0300

    Initial Commit

---
+++ To-Do-App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  To-Do-App
+//
+//  Created by Ecem Öztürk on 4.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ To-Do-App/To_Do_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_AppApp.swift
+//  To-Do-App
+//
+//  Created by Ecem Öztürk on 4.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
