3246e78ed36e762175947716a51535a979602aac
Ecem Ozturk <ecemozturk704@gmail.com>
2023-12-08 13:08:14 +0300

    added to do checkmark

--- To-Do-App/ViewModels/ToDoListItemViewModel.swift
+++ To-Do-App/ViewModels/ToDoListItemViewModel.swift
@@ -6,12 +6,22 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class ToDoListItemViewModel : ObservableObject {
 
     init(){}
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users").document(uid).collection("todos").document(item.id).setData(itemCopy.asDictionary())
     }
 }
